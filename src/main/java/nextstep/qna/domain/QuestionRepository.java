7117c1808df0f134ac3b5f76a9b6402be149be38
wooobo <irooori89@gmail.com>
2023-05-11 23:38:39 +0900

    refactor : 주석 제거

--- src/main/java/nextstep/qna/domain/QuestionRepository.java
+++ src/main/java/nextstep/qna/domain/QuestionRepository.java
@@ -3,7 +3,6 @@ package nextstep.qna.domain;
 import java.util.Optional;
 
 public interface QuestionRepository {
-    Optional<Question> findById(Long id);
 
-//    Optional<Question> findByIdAndDeletedFalse(Long id);
+    Optional<Question> findById(Long id);
 }
