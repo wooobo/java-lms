34edbbd78a4b59a6869dc3cfdb1868601fccdd89
wooobo <irooori89@gmail.com>
2023-05-11 23:49:38 +0900

    refactor : NsUser `@Deprecated` 메소드 제거 리팩토링

--- src/main/java/nextstep/users/domain/NsUser.java
+++ src/main/java/nextstep/users/domain/NsUser.java
@@ -3,9 +3,9 @@ package nextstep.users.domain;
 import nextstep.qna.UnAuthorizedException;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 public class NsUser {
+
     public static final GuestNsUser GUEST_USER = new GuestNsUser();
 
     private Long id;
@@ -29,7 +29,8 @@ public class NsUser {
         this(id, userId, password, name, email, LocalDateTime.now(), null);
     }
 
-    public NsUser(Long id, String userId, String password, String name, String email, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public NsUser(Long id, String userId, String password, String name, String email,
+        LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
         this.userId = userId;
         this.password = password;
@@ -39,32 +40,12 @@ public class NsUser {
         this.updatedAt = updatedAt;
     }
 
-    @Deprecated
-    public Long getId() {
-        return id;
-    }
-
-    @Deprecated
-    public String getUserId() {
-        return userId;
-    }
-
-    @Deprecated
-    public String getPassword() {
-        return password;
-    }
-
-    @Deprecated
-    public String getName() {
-        return name;
-    }
-
     public void update(NsUser loginUser, NsUser target) {
-        if (!matchUserId(loginUser.getUserId())) {
+        if (!matchUserId(String.valueOf(loginUser.id))) {
             throw new UnAuthorizedException();
         }
 
-        if (!matchPassword(target.getPassword())) {
+        if (!matchPassword(target.password)) {
             throw new UnAuthorizedException();
         }
 
@@ -85,6 +66,7 @@ public class NsUser {
     }
 
     private static class GuestNsUser extends NsUser {
+
         @Override
         public boolean isGuestUser() {
             return true;
@@ -94,12 +76,12 @@ public class NsUser {
     @Override
     public String toString() {
         return "NsUser{" +
-                "id=" + id +
-                ", userId='" + userId + '\'' +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                '}';
+            "id=" + id +
+            ", userId='" + userId + '\'' +
+            ", name='" + name + '\'' +
+            ", email='" + email + '\'' +
+            ", createdAt=" + createdAt +
+            ", updatedAt=" + updatedAt +
+            '}';
     }
 }
