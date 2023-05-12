2e7b26690c85d0039d78b90b824c22e83a329791
wooobo <irooori89@gmail.com>
2023-05-12 23:01:23 +0900

    feat : Course은 강의를 가질수 있다 적용

--- src/main/java/nextstep/courses/domain/Course.java
+++ src/main/java/nextstep/courses/domain/Course.java
@@ -1,8 +1,10 @@
 package nextstep.courses.domain;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Course {
+
     private Long id;
 
     private String title;
@@ -12,20 +14,42 @@ public class Course {
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
+    private Sessions sessions;
 
     private Course() {
     }
 
     public Course(String title, Long creatorId) {
-        this(0L, title, creatorId, LocalDateTime.now(), null);
+        this(0L, title, creatorId, LocalDateTime.now(), null, Sessions.empty());
     }
 
-    public Course(Long id, String title, Long creatorId, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    @Deprecated
+    public Course(Long id, String title, Long creatorId, LocalDateTime createdAt,
+        LocalDateTime updatedAt) {
         this.id = id;
         this.title = title;
         this.creatorId = creatorId;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+        this.sessions = Sessions.empty();
+    }
+
+    public Course(Long id, String title, Long creatorId, LocalDateTime createdAt,
+        LocalDateTime updatedAt, Sessions sessions) {
+        this.id = id;
+        this.title = title;
+        this.creatorId = creatorId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.sessions = sessions;
+    }
+
+    public void addSession(Session session) {
+        sessions.add(session);
+    }
+
+    public boolean hasSession(Session session) {
+        return sessions.contains(session);
     }
 
     @Deprecated
@@ -44,13 +68,22 @@ public class Course {
     }
 
     @Override
-    public String toString() {
-        return "Course{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", creatorId=" + creatorId +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                '}';
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (id == null) {
+            return false;
+        }
+        Course course = (Course) o;
+        return Objects.equals(id, course.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, creatorId, createdAt, updatedAt, sessions);
     }
 }
